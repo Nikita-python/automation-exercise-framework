36beb0e76e1a6e8a27c814287a3465a08424c5bb
Nikita-python <nikitaauti98@gmail.com>
2026-08-01 22:50:54 +0530

    converted sigunuppage file to TS

--- tests/Signup.spec.ts
+++ tests/Signup.spec.ts
@@ -1,14 +1,36 @@
-import {test,expect} from '@playwright/test'
-import { SignupPage } from '../Pages/SignupPage';
+import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
-import SignupData from '../Fixtures/SignupData.json' assert { type: 'json' };
+import { SignupPage } from '../Pages/SignupPage';
+import signupData from '../Fixtures/SignupData.json';
 
-test('Signup',async({page})=>{
+interface TestUser {
+    name: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    city: string;
+    state: string;
+    zipcode: string;
+    mobileNumber: string;
+}
+
+interface FixedSignupData {
+    title: string;
+    day: string;
+    month: string;
+    year: string;
+    country: string;
+}
+
+test('Signup', async ({ page }) => {
     const signupPage = new SignupPage(page);
-    const testUser={
-name:faker.person.firstName(),
-email:faker.internet.email(),
-password: faker.internet.password({ length: 8 }),
+
+    const testUser: TestUser = {
+        name: faker.person.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 8 }),
         firstName: faker.person.firstName(),
         lastName: faker.person.lastName(),
         address: faker.location.streetAddress(),
@@ -16,42 +38,48 @@ password: faker.internet.password({ length: 8 }),
         state: faker.location.state(),
         zipcode: faker.location.zipCode(),
         mobileNumber: faker.phone.number()
-    }
-    const fixedData = SignupData.validuser;
-    //Go To Signup page 
-await signupPage.goto();
-await expect(page.getByText('New User Signup!')).toBeVisible();
-//Initial signupForm
-await signupPage.enterName(testUser.name);
+    };
 
-await signupPage.enterEmail(testUser.email);
-await signupPage.submit();
-//Account Information page
-await expect(page.getByText('Enter Account Information')).toBeVisible();
-await signupPage.selectTitle(fixedData.title);
-await expect(signupPage.titleMrsRadio).toBeChecked();
-await expect(signupPage.accountInfoName).toHaveValue(testUser.name);
-await expect(signupPage.accountInfoEmail).toHaveValue(testUser.email);
-await signupPage.enterPass(testUser.password);
-await signupPage.selectDob(fixedData.day,fixedData.month,fixedData.year);
-await expect(signupPage.dayDropdown).toHaveValue(fixedData.day);
-await expect(signupPage.yearDropdown).toHaveValue(fixedData.year);
-await signupPage.checkNewsletter();
-await expect(signupPage.newsletterCheckbox).toBeChecked();
-await signupPage.checkSpecialOffers();
-await expect(signupPage.specialOffersCheckbox).toBeChecked();
-await signupPage.selectcountry(fixedData.country);
-await expect(signupPage.country).toHaveValue(fixedData.country);
-await signupPage.enterFirstName(testUser.firstName);
-await signupPage.enterLastName(testUser.lastName);
-await signupPage.enterAddress(testUser.address);
-await signupPage.enterState(testUser.state);
-await signupPage.enterCity(testUser.city);
-await signupPage.enterZipcode(testUser.zipcode);
-await signupPage.enterMobileNumber(testUser.mobileNumber);
+    const fixedData: FixedSignupData = signupData.validuser;
 
-await signupPage.createAccount();
+    // Go to Signup page
+    await signupPage.goto();
+    await expect(page.getByText('New User Signup!')).toBeVisible();
 
-await expect(page.getByText('ACCOUNT CREATED!')).toBeVisible();
-})
+    // Initial signup form
+    await signupPage.enterName(testUser.name);
+    await signupPage.enterEmail(testUser.email);
+    await signupPage.submit();
 
+    // Account Information page
+    await expect(page.getByText('Enter Account Information')).toBeVisible();
+    await signupPage.selectTitle(fixedData.title);
+    await expect(signupPage.titleMrsRadio).toBeChecked();
+
+    await expect(signupPage.accountInfoName).toHaveValue(testUser.name);
+    await expect(signupPage.accountInfoEmail).toHaveValue(testUser.email);
+
+    await signupPage.enterPass(testUser.password);
+    await signupPage.selectDob(fixedData.day, fixedData.month, fixedData.year);
+    await expect(signupPage.dayDropdown).toHaveValue(fixedData.day);
+    await expect(signupPage.yearDropdown).toHaveValue(fixedData.year);
+
+    await signupPage.checkNewsletter();
+    await expect(signupPage.newsletterCheckbox).toBeChecked();
+    await signupPage.checkSpecialOffers();
+    await expect(signupPage.specialOffersCheckbox).toBeChecked();
+
+    await signupPage.selectCountry(fixedData.country);
+    await expect(signupPage.country).toHaveValue(fixedData.country);
+
+    await signupPage.enterFirstName(testUser.firstName);
+    await signupPage.enterLastName(testUser.lastName);
+    await signupPage.enterAddress(testUser.address);
+    await signupPage.enterState(testUser.state);
+    await signupPage.enterCity(testUser.city);
+    await signupPage.enterZipcode(testUser.zipcode);
+    await signupPage.enterMobileNumber(testUser.mobileNumber);
+
+    await signupPage.createAccount();
+    await expect(page.getByText('ACCOUNT CREATED!')).toBeVisible();
+});
